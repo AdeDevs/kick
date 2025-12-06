e091de6d622479d7966488c8d7274ad762d18c46
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-12-06 13:26:05 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
 
 import 'swiper/css';
+// import { motion } from 'framer-motion';
 // import 'swiper/css/effect-fade';
 
 export default function HomePage() {
@@ -26,9 +27,74 @@ export default function HomePage() {
                 </Swiper>
 
             </header>
+            <div className="events">
+                <h1>top events</h1>
+                <div className="event-results">
+                    <div>
+                        <h2>final</h2>
+                        <ul className="event-score">
+                            <section className="check"> <li><span className="color"></span> che</li>  <span className='num'>5</span> </section>
+                            <section className='check'><li><span className="color"></span> lee </li><span className='num'>0</span></section>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>final</h2>
+                        <ul className="event-score">
+                            <section className="check"> <li><span className="color"></span> ars</li>  <span className='num'>1</span> </section>
+                            <section className='check'><li><span className="color"></span> mci </li><span className='num'>1</span></section>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>final</h2>
+                        <ul className="event-score">
+                            <section className="check"> <li><span className="color"></span> mun</li>  <span className='num'>2</span> </section>
+                            <section className='check'><li><span className="color"></span> eve </li><span className='num'>5</span></section>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>final</h2>
+                        <ul className="event-score">
+                            <section className="check"> <li><span className="color"></span> lei</li>  <span className='num'>3</span> </section>
+                            <section className='check'><li><span className="color"></span> liv </li><span className='num'>1</span></section>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>final</h2>
+                        <ul className="event-score">
+                            <section className="check"> <li><span className="color"></span> bar</li>  <span className='num'>2</span> </section>
+                            <section className='check'><li><span className="color"></span> rma </li><span className='num'>2</span></section>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>final</h2>
+                        <ul className="event-score">
+                            <section className="check"> <li><span className="color"></span> mun</li>  <span className='num'>2</span> </section>
+                            <section className='check'><li><span className="color"></span> ars </li><span className='num'>0</span></section>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>final</h2>
+                        <ul className="event-score">
+                            <section className="check"> <li><span className="color"></span> mun</li>  <span className='num'>2</span> </section>
+                            <section className='check'><li><span className="color"></span> ars </li><span className='num'>0</span></section>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>final</h2>
+                        <ul className="event-score">
+                            <section className="check"> <li><span className="color"></span> mun</li>  <span className='num'>2</span> </section>
+                            <section className='check'><li><span className="color"></span> ars </li><span className='num'>0</span></section>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <h1>Hello World</h1>
         </div>
     )
 }
+
+
 
 function Stream() {
     return (
